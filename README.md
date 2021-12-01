192d2a5c1c5cf5e17e0047245320bb7bf71beb29
Javier <ibrahimcruz06@gmail.com>
2021-11-30 18:13:51 -0600

    Creacion de "Hello" y readme, tarea terminada

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Tarea-programada-IV
 Javier Cruz C01517, programacion 2 grupo 02,  tarea programada 4 
+
+El código se compila y se ejecuta con main.exe. 
+La respuesta a la tarea consiste en los archivos operacionSuma.h y operacionSuma.cpp, así como la implementación 
+de un nuevo ifstream en el main.cpp para obtener el formato que da el usuario. 
--- src/main.cpp
+++ src/main.cpp
@@ -29,13 +29,25 @@ int main() {
             return -1;
         } 
 
+        string nombreArchivo_deUsuario = "formatoIngles.ini"; //esta variable se puede cambiar dependiendo del usuario
+
+        ifstream archivoUsuario(nombreArchivo_deUsuario, ifstream::in); 
+        if(!archivoUsuario.is_open()){
+            std::cerr << "Error leyendo archivo formatoUsuario.ini" << std::endl;
+            return -1;
+        }
+
         ProveedorFormato *proveedorFormato = new ProveedorFormatoDesdeStream(&ifs);
+        ProveedorFormato *proveedorFormatoUsuario = new ProveedorFormatoDesdeStream(&archivoUsuario); 
 
         OperacionHola *operacionHola = new OperacionHola(proveedorFormato);
         OperacionSuma *operacionSuma = new OperacionSuma(); 
+        OperacionHola *operacionHello = new OperacionHola(proveedorFormatoUsuario); 
         
         operaciones.insert(std::pair<string, Operacion *>("hola", operacionHola));
         operaciones.insert(std::pair<string, Operacion *>("suma", operacionSuma)); 
+        operaciones.insert(std::pair<string, Operacion *>("hello", operacionHello)); 
+
 
         IdentificadorOperacionesBase *identificadorOperaciones = new IdentificadorOperaciones(operaciones);
 
@@ -43,8 +55,10 @@ int main() {
         ProcesadorInstrucciones *procesador = new ProcesadorInstrucciones(identificadorOperaciones);
         string resultado = procesador->Procese("hola", "todos");
         string salida = procesador->Procese("suma","2 + 3"); 
-        cout << "Resultado: " << salida << endl; 
-        cout << "Resultado: " << resultado << endl; 
+        string resultadoHello = procesador->Procese("hello", "it's me"); 
+        cout << "Resultado operacionSuma: " << salida << endl; 
+        cout << "Resultado operacionHola: " << resultado << endl; 
+        cout << "Resultado hello: " << resultadoHello << endl; 
 
         // Cerrar archivo de entrada
         ifs.close();
@@ -53,6 +67,7 @@ int main() {
     {
         cerr << "Error: " << exception << endl;
     }
+    
 
     return 0;
 }
